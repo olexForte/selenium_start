e841611d1a580c4f4cd074ff8b61e069424c33d5
ben-laird <86321957+ben-laird@users.noreply.github.com>
2021-07-06 15:09:52 -0500

    Added in Selenium error handling and fully tested

--- src/main/java/automation/Main.java
+++ src/main/java/automation/Main.java
@@ -6,20 +6,22 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 public class Main {
     public static void main(String[] args) {
-        System.setProperty("webdriver.chrome.driver", "/Users/benjaminlaird/Desktop/ForteWorkspace/chromedriver");
+        System.setProperty("webdriver.chrome.driver", "/Users/benjaminlaird/Desktop/ForteWorkspace/selenium_start/chromedriver");
         WebDriver driver = new ChromeDriver();
         driver.get("https://www.costco.com"); //visits Costco website
-        driver.findElement(By.id("search-field")).sendKeys("rotisserie chicken");
+        driver.findElement(By.id("search-field")).sendKeys("plastic spoon");
         driver.findElement(By.cssSelector("button.btn.search-ico-button")).click();
-//         try
-//             {
-//                 Thread.sleep(1000);
-//             }
-//             catch(InterruptedException ex)
-//             {
-//                 Thread.currentThread().interrupt();
-//             }
-         driver.close();
-         driver.quit();
+        try {
+            boolean isVisible = driver.findElement(By.cssSelector("#search-results > div.product-list.grid > div:nth-child(1) > div.product-tile-set > div.thumbnail > div.caption.link-behavior > div.caption > span > a")).isDisplayed();
+            if(isVisible){
+                System.out.println("The element is visible. Success!");
+            } else {
+                System.out.println("The element is not visible.");
+            }
+        } catch (Exception NoSuchElementException){
+            System.out.println("We couldn't find an element matching the criteria");
+        }
+        driver.close();
+        driver.quit();
     }
 }
