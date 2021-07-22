c7296cc42db60cdb2d885799a033a087013f0ccc
ben-laird <86321957+ben-laird@users.noreply.github.com>
2021-07-22 15:56:31 -0500

    Adds initial tests

--- src/test/java/Test1.java
+++ src/test/java/Test1.java
@@ -1,26 +1,34 @@
-import static org.junit.Assert.assertEquals;
-import org.junit.Test;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.*;
 
-import automation.Page;
+import automation.*;
 
 public class Test1 {
     
-    static Page page;
+    static SearchPage page;
 
     @BeforeEach
-    public static void setup(){
-        page = new Page("https://costco.com", "chromeCap");
+    public void setup(){
+        page = new SearchPage("https://costco.com", "chromeCap", "#search-field", "plastic spoon", "button.btn.search-ico-button");
     }
 
     @Test
     public void visit (){
-        assertEquals("Costco was not visited!", "Welcome to Costco Wholesale", page.driver.getTitle());
+        Assertions.assertEquals("Welcome to Costco Wholesale", page.driver.getTitle(), "Costco was not visited!");
+    }
+
+    @Test
+    public void searchTest (){
+        page.search(page.objQuery);
+
+        Integer expectExitCode = 0;
+        Integer actualExitCode = page.verify("#search-results > div.product-list.grid > div:nth-child(1) > div.product-tile-set > div.thumbnail > div.caption.link-behavior > div.caption > span > a");
+        
+        Assertions.assertEquals(expectExitCode, actualExitCode);
     }
 
     @AfterEach
-    public static void teardown (){
+    public void teardown (){
+        page.closeTab();
         page.stop();
     }
 }
